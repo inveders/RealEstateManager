6735e5277eb18534b29af102da48d8ea1211542d
inveders <alexandra.gnimadi@gmail.com>
2019-09-03 10:49:45 +0200

    Creating MainApplication class to add JodaTime and Stetho.

---
+++ app/src/main/java/com/inved/realestatemanager/MainApplication.java
@@ -0,0 +1,17 @@
+package com.inved.realestatemanager;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JodaTimeAndroid.init(this);
+        Stetho.initializeWithDefaults(this);
+
+    }
+}
