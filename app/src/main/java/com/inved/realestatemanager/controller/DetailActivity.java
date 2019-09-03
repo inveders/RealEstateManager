120a97e779cc5f937c9a424a526ab5326f8dd490
inveders <alexandra.gnimadi@gmail.com>
2019-09-03 10:52:53 +0200

    Adding DetailActivity, MapsActivity and ProfileActvity

---
+++ app/src/main/java/com/inved/realestatemanager/controller/DetailActivity.java
@@ -0,0 +1,44 @@
+package com.inved.realestatemanager.controller;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.testimmo.R;
+import com.example.testimmo.base.BaseActivity;
+import com.example.testimmo.property.DetailPropertyFragment;
+
+public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.configureAndShowDetailFragment();
+    }
+
+    // Declare detail fragment
+    private DetailPropertyFragment detailPropertyFragment;
+
+    @Override
+    protected int getLayoutContentViewID() {
+        return R.layout.activity_detail;
+    }
+
+    // --------------
+    // FRAGMENTS
+    // --------------
+
+    private void configureAndShowDetailFragment(){
+        // A - Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        detailPropertyFragment = (DetailPropertyFragment) getSupportFragmentManager().findFragmentById(R.id.activity_detail_frame_layout);
+
+        if (detailPropertyFragment == null) {
+            // B - Create new detail property fragment
+            detailPropertyFragment = new DetailPropertyFragment();
+            // C - Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_detail_frame_layout, detailPropertyFragment)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/inved/realestatemanager/controller/ProfileActivity.java
@@ -0,0 +1,11 @@
+package com.inved.realestatemanager.controller;
+
+import com.example.testimmo.R;
+import com.example.testimmo.base.BaseActivity;
+
+public class ProfileActivity extends BaseActivity {
+    @Override
+    protected int getLayoutContentViewID() {
+        return R.layout.activity_profile;
+    }
+}
