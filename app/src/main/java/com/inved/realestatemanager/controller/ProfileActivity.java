546bf3066154c3ba86e85b5e7b5c93f3a64d98e1
inveders <alexandra.gnimadi@gmail.com>
2019-09-03 10:59:20 +0200

    Adding activity_main xml to smartphone and tablet (layout-sw700dp)

--- app/src/main/java/com/inved/realestatemanager/controller/ProfileActivity.java
+++ app/src/main/java/com/inved/realestatemanager/controller/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.inved.realestatemanager.controller;
 
 import com.example.testimmo.R;
 import com.example.testimmo.base.BaseActivity;
+import com.inved.realestatemanager.R;
+import com.inved.realestatemanager.base.BaseActivity;
 
 public class ProfileActivity extends BaseActivity {
     @Override
