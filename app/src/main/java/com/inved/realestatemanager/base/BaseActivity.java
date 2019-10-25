600b85cbaf2465fc50e993478a8723a139cc2d12
inveders <alexandra.gnimadi@gmail.com>
2019-10-25 16:41:39 +0200

    BaseActivity

--- app/src/main/java/com/inved/realestatemanager/base/BaseActivity.java
+++ app/src/main/java/com/inved/realestatemanager/base/BaseActivity.java
@@ -1,15 +1,27 @@
 package com.inved.realestatemanager.base;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import butterknife.ButterKnife;
+import com.inved.realestatemanager.R;
+import com.inved.realestatemanager.controller.activity.CreatePropertyActivity;
 
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     // --------------------
     // LIFE CYCLE
@@ -19,7 +31,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutContentViewID());
-        ButterKnife.bind(this); //Configure Butterknife
+//        ButterKnife.bind(this); //Configure Butterknife
     }
 
     protected abstract int getLayoutContentViewID();
@@ -28,9 +40,14 @@ public abstract class BaseActivity extends AppCompatActivity {
     // UI
     // --------------------
 
-    protected void configureToolbar() {
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+    protected void configToolbar(){
+        this.toolbar = findViewById(R.id.toolbar);
+        Log.d("debago","in config toolbar Base Activity");
+        setSupportActionBar(toolbar);
+        setTitle("HOME");
     }
+
+
+
 }
 
