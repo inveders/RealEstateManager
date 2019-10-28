3c8dbed2fefe1b12028948b6c237c93b637a7c0f
inveders <alexandra.gnimadi@gmail.com>
2019-10-28 10:07:56 +0100

    CreateUpdateChoice in sharedpreferences and enum to list differents possibles choices

---
+++ app/src/main/java/com/inved/realestatemanager/utils/CreateUpdateChoice.java
@@ -0,0 +1,6 @@
+package com.inved.realestatemanager.utils;
+
+public enum CreateUpdateChoice {
+    CREATE,
+    UPDATE,
+}
---
+++ app/src/main/java/com/inved/realestatemanager/utils/ManageCreateUpdateChoice.java
@@ -0,0 +1,22 @@
+package com.inved.realestatemanager.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class ManageCreateUpdateChoice {
+
+    private static final String KEY_CREATE_UPDATE_CHOICE = "KEY_CREATE_UPDATE_CHOICE";
+    private static final String KEY_CREATE_UPDATE_CHOICE_DATA = "KEY_CREATE_UPDATE_CHOICE_DATA";
+
+    public static void saveCreateUpdateChoice(Context context, String createUpdateChoice) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_CREATE_UPDATE_CHOICE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_CREATE_UPDATE_CHOICE_DATA, createUpdateChoice);
+        editor.apply();
+    }
+
+    public static String getCreateUpdateChoice(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(KEY_CREATE_UPDATE_CHOICE, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_CREATE_UPDATE_CHOICE_DATA,CreateUpdateChoice.CREATE.toString());
+    }
+}
