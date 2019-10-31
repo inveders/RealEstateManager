0ea50e707a1d9d758d886a887da84dacb7c71ca1
inveders <alexandra.gnimadi@gmail.com>
2019-10-31 10:01:06 +0100

    Working on date picker style, and create update xml style creating specific styles in styles.xml

--- app/src/main/java/com/inved/realestatemanager/controller/fragment/DatePickerFragment.java
+++ app/src/main/java/com/inved/realestatemanager/controller/fragment/DatePickerFragment.java
@@ -1,6 +1,7 @@
 package com.inved.realestatemanager.controller.fragment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
@@ -11,6 +12,7 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.fragment.app.DialogFragment;
 
@@ -23,7 +25,6 @@ import java.util.Locale;
 public class DatePickerFragment extends AppCompatDialogFragment implements DatePickerDialog.OnDateSetListener, CreateUpdatePropertyFragmentTwo.OnFragmentInteractionListener {
 
     private static final String TAG = "debago";
-    private TextView dateTextView;
     private final Calendar calendar = Calendar.getInstance();
 
     @Override
@@ -33,7 +34,7 @@ public class DatePickerFragment extends AppCompatDialogFragment implements DateP
             int yy = calendar.get(Calendar.YEAR);
             int mm = calendar.get(Calendar.MONTH);
             int dd = calendar.get(Calendar.DAY_OF_MONTH);
-            return new DatePickerDialog(getActivity(), R.style.MyDatePickerStyle,DatePickerFragment.this, yy, mm, dd);
+            return new DatePickerDialog(getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_DARK,DatePickerFragment.this, yy, mm, dd);
         }
         return null;
     }
