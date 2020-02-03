082386d528fdc95d144169e5d003c3ee39d8a156
inveders <alexandra.gnimadi@gmail.com>
2020-02-03 18:11:36 +0100

    JoinCheckBoxInString test

---
+++ app/src/test/java/com/inved/realestatemanager/JoinCheckBoxInStringTest.java
@@ -0,0 +1,35 @@
+package com.inved.realestatemanager;
+
+import com.inved.realestatemanager.domain.JoinCheckBoxInString;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JoinCheckBoxInStringTest {
+
+    private JoinCheckBoxInString joinCheckBoxInString = Mockito.spy(new JoinCheckBoxInString());
+
+    @Test
+    public void should_ShowString_WithListString() {
+
+        //Given
+        List<String> list = new ArrayList<>();
+        list.add("Paris");
+        list.add("Luxembourg");
+        list.add("Monaco");
+        list.add("Washington");
+
+        //When
+        String convertedList = joinCheckBoxInString.joinMethod(list);
+
+        //Then
+
+        Assert.assertEquals("Paris,Luxembourg,Monaco,Washington",convertedList);
+
+    }
+
+}
