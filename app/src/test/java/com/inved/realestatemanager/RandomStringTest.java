419df6b3e4c1395dc10e35d05ceb36926b5009e5
inveders <alexandra.gnimadi@gmail.com>
2020-02-04 10:24:10 +0100

    RandomString test on lenght

---
+++ app/src/test/java/com/inved/realestatemanager/RandomStringTest.java
@@ -0,0 +1,28 @@
+package com.inved.realestatemanager;
+
+import com.inved.realestatemanager.domain.SplitString;
+import com.inved.realestatemanager.utils.RandomString;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class RandomStringTest {
+
+    private RandomString randomString = Mockito.spy(new RandomString());
+
+    @Test
+    public void should_GiveString_When_CreateRandomString_With_20Characters() {
+
+        //Given
+        int lenghtToRetrieve = 20;
+
+        //When
+        String myNewString = randomString.generateRandomString();
+        int myNewStringLenght = myNewString.length();
+        //Then
+
+        Assert.assertEquals(lenghtToRetrieve, myNewStringLenght);
+
+    }
+}
